20d05d47e6cd66f43dd7d18ad9acf1a7dcb8e66c
DillPickle80 <67298238+DillPickle80@users.noreply.github.com>
2020-12-07 04:28:35 -0500

    Clean

--- src/components/commons/calendar.jsx
+++ src/components/commons/calendar.jsx
@@ -7,34 +7,16 @@ class Calendar extends Component {
     events: [],
   };
 
-  // async getEventsFromApi() {
-  //   try {
-  //     let response = await fetch(
-  //       "https://node-event-service-dylanl.nn.r.appspot.com/events"
-  //     );
-  //     let responseJson = await response.json();
-  //     console.log(responseJson);
-  //     return responseJson;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
   componentDidMount() {
     var request = new Request(
       "https://node-event-service-dylanl.nn.r.appspot.com/events"
     );
-    // request.headers
 
     fetch(request, { mode: "cors" })
-      // fetch("https://node-event-service-dylanl.nn.r.appspot.com/events", {
-      //   mode: "cors",
-      // })
       .then((response) => response.json())
       .then((jsonEvents) => {
         // jsonData is parsed json object received from url
         const events = jsonEvents;
-        // events = jsonEvents;
         this.setState({ events });
         console.log(jsonEvents);
       })
@@ -42,7 +24,6 @@ class Calendar extends Component {
         // handle your errors here
         console.error(error);
       });
-    // const events = this.getEventsFromApi();
   }
 
   render() {
